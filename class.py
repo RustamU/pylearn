ec9654fde0204c652297e0d0502f033e21a36a3f
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-08-27 11:52:02 +0300

    add class

---
+++ class.py
@@ -0,0 +1,21 @@
+import sys
+import os as so
+
+
+class Person:
+
+    name = None
+    age = None
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+    def print_name(self):
+        return self.name
+
+    def print_age(self):
+        return self.age
+
+me = Person('john', 32)
+print ('My name is ', me.name)
+print ('My age is', me.age)
