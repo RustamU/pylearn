88d1ec9c985c13042a171ebfd98b5d05c683d8a6
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-08-31 17:53:35 +0300

    Problem set 1 v.3

--- Week1Problem3.py
+++ Week1Problem3.py
@@ -1,14 +1,13 @@
 s = 'azcbobobegghakl'
 longestSubstring = s[0]
-tempSubstring = ''
+tempSubstring = s[0]
 for i in range (len(s)-1):
     if s[i+1] >= s[i]:
         tempSubstring += s[i+1]
         i += 1
         if len(tempSubstring) > len(longestSubstring):
             longestSubstring = tempSubstring
-
     else:
         tempSubstring = s[i+1]
         i += 1
-print ('Longest substring in alphabetical order is:', longestSubstring)
+print ('Longest substring in alphabetical order is: ', longestSubstring)
