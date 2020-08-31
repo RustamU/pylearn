0f8e06d750b61547bbeeec44e52f12bd57d769e6
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-08-31 17:41:45 +0300

    ProblemSet 1 is done

--- Week1Problem3.py
+++ Week1Problem3.py
@@ -1,8 +1,15 @@
-s = 'azcbobobegghakl'
+s = 'abcdefabcdefghz'
 longestSubstring = s[0]
+tempSubstring = ''
 for i in range (len(s)-1):
-    while s[i+1] > s[i]:
-        if i == len(s):
-            break
-        longestSubstring += s[i+1]
+    if s[i+1] >= s[i]:
+        tempSubstring += s[i+1]
+        i += 1
+        if len(tempSubstring) > len(longestSubstring):
+            longestSubstring = tempSubstring
         print (longestSubstring)
+    else:
+        tempSubstring = s[i+1]
+        i += 1
+        print (longestSubstring)
+print (longestSubstring)
