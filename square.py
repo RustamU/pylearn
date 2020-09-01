f76b2087f5998eba52ef7a50383c6c2e44bbf63e
RustamU <rustam.urusov@gmail.com>
2020-09-01 22:51:55 +0300

    99

--- square.py
+++ square.py
@@ -1,7 +1,7 @@
 number = float(input ('Enter number: '))
 ans = number
 iter = 1
-while abs(ans**2 - number) > 0.01:
+while abs(ans**2 - number) > 0.0001:
     ans = (ans + number/ans)/2
     iter += 1
 print (ans, ans**2, iter)
