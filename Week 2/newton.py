8fc9392e1489a0bfff7b3bc2ce56ce323f72f9e9
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-04 17:00:47 +0300

    +Newton

--- Week 2/newton.py
+++ Week 2/newton.py
@@ -1,8 +1,8 @@
 while True:
-    number = float(input ('Geron Enter number: '))
+    number = float(input ('Newton Enter number: '))
     ans = number
     iter = 1
     while abs(ans**2 - number) > 0.01:
-        ans = (ans + number/ans)/2
+        ans = ans - (((ans**2) - number)/(2*ans))
         iter += 1
     print (ans, ans**2, iter)
