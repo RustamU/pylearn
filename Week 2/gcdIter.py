c59f4bffd6c6918b24ba6f293bdc85283119e592
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-08 18:02:42 +0300

    11

--- Week 2/gcdIter.py
+++ Week 2/gcdIter.py
@@ -4,4 +4,11 @@ def gcdIter(a, b):
 
     returns: a positive integer, the greatest common divisor of a & b.
     '''
-    # Your code here
+    gcd = 1
+    lowest = min(a,b)
+    for i in range (1, lowest +1):
+        if a % i == 0 and b % i == 0 and i > gcd:
+            gcd = i
+    return gcd
+
+print (gcdIter(19,12))
