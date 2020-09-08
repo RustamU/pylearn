b023baeeb6379279c28e47dda0629eab1a50029d
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-08 14:27:52 +0300

    Hanoj + recursion

--- Week 2/func1.py
+++ Week 2/func1.py
@@ -1,3 +1,29 @@
-a = "number one - the larch"
-a = a.capitalize()
-print (a)
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    result = 1
+    while exp > 0:
+        result = result * base
+        exp -= 1
+    return result
+
+print ("ITER", iterPower (3,3))
+
+def recurPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    if exp == 0:
+        return 1
+    else:
+        return base*recurPower(base, exp-1 )
+
+print ("RECUR", recurPower (3,3))
