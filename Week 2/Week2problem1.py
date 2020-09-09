31e1b46bd1315a11fdefdb57968ade41931a0af5
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-09 20:38:13 +0300

    week problem 1

---
+++ Week 2/Week2problem1.py
@@ -0,0 +1,19 @@
+def balanceCalc(balance, annualInterestRate, monthlyPaymentRate):
+    """
+    function prints year credit balance based on minimum monthly monthly payment
+    rounded to two decimal digits
+    balance - initial balance, float or int
+    annualInterestRate - annual interest, float
+    monthlyPaymentRate - minimum possible part of debt paid monthly, float
+    tBal - variable for store current balance, float
+    """
+    tBal = balance
+    i = 1
+    while i <= 12:
+        tBal = ((tBal - tBal*monthlyPaymentRate) + (tBal - tBal*monthlyPaymentRate)*annualInterestRate/12)
+        i += 1
+    return round (tBal, 2)
+balance = 484
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+print ('Remaining balance:', balanceCalc (balance, annualInterestRate, monthlyPaymentRate))
