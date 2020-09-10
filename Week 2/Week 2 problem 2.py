23d6bbb84bfcb42f52ec11d842dbb241a3391fbc
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-10 13:13:29 +0300

    + week 2 problem 2 done

--- Week 2/Week 2 problem 2.py
+++ Week 2/Week 2 problem 2.py
@@ -4,5 +4,26 @@ def lowestPayment (balance, annualInterestRate):
     of 10 enough to pay all debt in 12 months
     balance - initial debt, int or float
     annualInterestRate - float i.e. 0.12
+    mpr - monthly payment rate
     """
-    
+    tBal = balance
+    air = annualInterestRate
+    mpr = 10
+
+    while balanceCalc (tBal, air, mpr) > 0:
+        mpr += 10
+    return mpr
+
+
+def balanceCalc(tBal, air, mpr):
+    """
+    calculating balance with testing montly payment
+    returning balance after 12 months
+    """
+    i = 1
+    while i <= 12:
+        tBal = ((tBal - mpr) + (tBal - mpr)*air/12)
+        i += 1
+    return tBal
+
+print ("Lowest Payment:", lowestPayment(3379, 0.2))
