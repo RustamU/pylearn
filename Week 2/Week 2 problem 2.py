983aed1dcaae8db501dd71fa905faccb05d592ad
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-10 11:40:31 +0300

    + week 2 problem 2

---
+++ Week 2/Week 2 problem 2.py
@@ -0,0 +1,8 @@
+def lowestPayment (balance, annualInterestRate):
+    """
+    lowestPayment function calculates minimum monthly payment, which is multiply
+    of 10 enough to pay all debt in 12 months
+    balance - initial debt, int or float
+    annualInterestRate - float i.e. 0.12
+    """
+    
