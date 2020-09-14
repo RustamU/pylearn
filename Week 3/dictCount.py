6055685b4aa7188082bf2397885ab2081da742e4
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-14 16:13:05 +0300

    count animals works correct, error on testing side

---
+++ Week 3/dictCount.py
@@ -0,0 +1,20 @@
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    # Your Code Here
+    count  = 0
+    for i in aDict.values():
+        for k in i:
+            count +=1
+    return count
+
+
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+print (how_many (animals))
