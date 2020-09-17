ef18e97d02d01ffcd323210289776f77cb138920
Урусов Рустам Рашидович <urusov@swgroup.ru>
2020-09-17 11:33:34 +0300

    One line!

--- Week 3/Week 3 Problem 1.py
+++ Week 3/Week 3 Problem 1.py
@@ -1,10 +1,17 @@
 def isWordGuessed(secretWord, lettersGuessed):
-    print ('secretWord = ', secretWord)
-    print ('lettersGuessed =', lettersGuessed )
-    guess = True
-    for a in secretWord:
-        if a not in lettersGuessed:
-            guess = False
-    return guess
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+# This code works
+    # guess = True
+    # for a in secretWord:
+    #     if a not in lettersGuessed:
+    #         guess = False
+    # return guess
+# But this code is better
+    return False not in [a in lettersGuessed for a in secretWord]
 
-print (isWordGuessed('ab', ['c', 'a', 'b']))
+print (isWordGuessed('abzsfw', ['c', 'a', 'b', 'z','s','f','w']))
