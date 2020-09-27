a72f11ae1228a79673a255f6752603a7ff293d58
RustamU <rustam.urusov@gmail.com>
2020-09-27 17:11:36 +0300

    11

---
+++ Week 4/rem.py
@@ -0,0 +1,26 @@
+def rem(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the remainder when x is divided by a.
+    """
+    if x == a:
+        return 0
+    elif x < a:
+        return x
+    else:
+        return rem(x-a, a)
+
+def f(n):
+   """
+   n: integer, n >= 0.
+   """
+   if n == 0:
+      return 1
+   else:
+      return n * f(n-1)
+
+print (f(3))
+print (f(1))
+print (f(0))
